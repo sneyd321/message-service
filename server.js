2dbbd9c595da7e2377b8a30552a2458cf86ec337
Ryan Sneyd <rts1234567@hotmail.com>
2021-12-18 11:37:20 -0500

    update git

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var http = require("http");
 var socketIO = require("socket.io");
 const mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
 
 
-mongoose.connect('mongodb://mongo-service.default.svc.cluster.local:27017/roomr', {useNewUrlParser: true});
+mongoose.connect('mongodb://mongo-service.default.svc.cluster.local:27017/roomr', {useNewUrlParser: true, poolSize: 100});
 
-var server = http.createServer()
-server.listen(8087, "0.0.0.0");
+//mongoose.connect('mongodb://host.docker.internal:27017/roomr', {useNewUrlParser: true, poolSize: 100});
+
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -17,20 +24,24 @@ const messageSchema = new mongoose.Schema({
     email: String,
     userType: String,
     userName: String,
+    imageURL: String,
     houseId: Number
 });
 const Model = mongoose.model('Message', messageSchema, "messages");
 
 
   
-
+var server = app.listen(8087, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+ 
 
 
 
 io = socketIO(server);
-
-
-
 
 
 
